218a1ac99be3526e1e3819122268c13039a9e057
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-02-28 12:13:04 +0200

    add screenshoot

--- app/src/main/java/com/app/educa/ui/view/ConferFragment.kt
+++ app/src/main/java/com/app/educa/ui/view/ConferFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.app.educa.databinding.FragmentGalleryBinding
+import com.app.educa.databinding.*
 import com.app.educa.ui.viewmodel.GalleryViewModel
 
-class GalleryFragment : Fragment() {
+class ConferFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentConferBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,12 +26,11 @@ class GalleryFragment : Fragment() {
         val galleryViewModel =
             ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentConferBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
         galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
         }
         return root
     }
