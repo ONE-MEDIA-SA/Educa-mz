78e0a1fca0c75332459611d9b38597b67465c461
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-02 15:44:34 +0200

    add gallery

---
+++ app/src/main/java/com/app/educa/ui/view/GalleryFragment.kt
@@ -0,0 +1,44 @@
+package com.app.educa.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.app.educa.databinding.FragmentGalleryBinding
+import com.app.educa.databinding.FragmentHomeBinding
+import com.app.educa.ui.viewmodel.HomeViewModel
+
+class GalleryFragment : Fragment() {
+
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        //val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+           // textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
