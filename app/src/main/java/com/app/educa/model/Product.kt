242e3740f7ed1826ef3622469a860beefe94cb45
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-10 11:44:11 +0200

    CREATE: list product adapter

---
+++ app/src/main/java/com/app/educa/model/Product.kt
@@ -0,0 +1,9 @@
+package com.app.educa.model
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val image: String,
+    val category: String)
---
+++ app/src/main/java/com/app/educa/ui/adapter/ProductAdapter.kt
@@ -0,0 +1,48 @@
+package com.app.educa.ui.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.app.educa.databinding.ItemProductHBinding
+import com.app.educa.model.Product
+import com.bumptech.glide.Glide
+
+class ProductAdapter : ListAdapter<Product, ProductAdapter.ProductViewHolder>(Companion) {
+
+    companion object : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    inner class ProductViewHolder(val binding: ItemProductHBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder(ItemProductHBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        Glide
+            .with(holder.binding.root)
+            .load(currentList[position])
+            .into(holder.binding.imageProduct)
+        holder.binding.tvProductName.text = currentList[position].name
+        holder.binding.tvProductPrice.text = """${currentList[position].price} MZN"""
+        holder.binding.root.setOnClickListener {
+            Toast.makeText(holder.binding.root.context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
