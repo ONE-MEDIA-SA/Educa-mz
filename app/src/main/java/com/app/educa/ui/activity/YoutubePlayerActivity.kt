d5c80bdd824a3275144c6ec1297edcceb74d396c
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-15 16:34:15 +0200

    :technologist: youtube player

---
+++ app/src/main/java/com/app/educa/ui/activity/YoutubePlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.app.educa.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.educa.R
+
+class YoutubePlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube_player)
+    }
+}
