5dc4cc4698bc79dfacaa9d691dae08c552aa0c2e
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-18 15:23:25 +0200

    update schedule

--- app/src/main/java/com/app/educa/utils/ScheduleUtils.kt
+++ app/src/main/java/com/app/educa/utils/ScheduleUtils.kt
@@ -1,11 +1,19 @@
 package com.app.educa.utils
 
+import android.graphics.Color
 import com.app.educa.R
 
 class ScheduleUtils {
-    fun randomColor(){
-        val colors = arrayOf(R.color.colorPrimary, R.color.colorPrimaryDark, R.color.colorAccent)
-        val random = (0..2).random()
-        return colors[random]
+    @kotlin.jvm.JvmField
+    val hsvColors = arrayOf(
+        floatArrayOf(11f, 97f, 58f),
+        floatArrayOf(145f, 100f, 55f),
+        floatArrayOf(41f, 100f, 70f),
+        floatArrayOf(247f, 100f, 64f),
+        floatArrayOf(30f, 89f, 53f)
+    )
+
+    fun lessBrightColor(color: FloatArray): Int {
+        return Color.HSVToColor(floatArrayOf(color[0], 0.2f, color[2]))
     }
 }
