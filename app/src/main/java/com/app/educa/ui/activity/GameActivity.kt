3a68cdfdd5114924efa56c6dd3ff86b200319fae
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-21 14:32:39 +0200

    add Question

--- app/src/main/java/com/app/educa/ui/activity/GameActivity.kt
+++ app/src/main/java/com/app/educa/ui/activity/GameActivity.kt
@@ -3,19 +3,53 @@ package com.app.educa.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.app.educa.R
+import com.app.educa.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityGameBinding
+    lateinit var tv_question: TextView
+    lateinit var tv_score: TextView
+    lateinit var tv_win_count: TextView
+    lateinit var tv_lost_count: TextView
+    lateinit var tv_option_1: TextView
+    lateinit var tv_option_2: TextView
+    lateinit var tv_option_3: TextView
+    lateinit var tv_option_4: TextView
+    lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        initViews()
+        handleClick()
+
+    }
+
+    private fun handleClick() {
+        tv_option_1.setOnClickListener { this }
+        tv_option_2.setOnClickListener { this }
+        tv_option_3.setOnClickListener { this }
+        tv_option_4.setOnClickListener { this }
+    }
+
+    private fun initViews() {
+        tv_question = binding.tvQuestion
+        tv_score = binding.tvScore
+        tv_win_count = binding.tvWinCount
+        tv_lost_count = binding.tvLostCount
+        tv_option_1 = binding.tvOption1
+        tv_option_2 = binding.tvOption2
+        tv_option_3 = binding.tvOption3
+        tv_option_4 = binding.tvOption4
+        progressBar = binding.pbScore
     }
 }
