7963cc072768aedde58f017444e288627f2da2ff
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-23 14:33:45 +0200

    list more user products

--- app/src/main/java/com/app/educa/ui/activity/ProductDetailsActivity.kt
+++ app/src/main/java/com/app/educa/ui/activity/ProductDetailsActivity.kt
@@ -2,11 +2,25 @@ package com.app.educa.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.app.educa.R
+import com.app.educa.databinding.ActivityProductDetailsBinding
+import com.app.educa.ui.adapter.ProductAdapter
+import com.app.educa.ui.viewmodel.ProductViewModel
 
 class ProductDetailsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProductDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_details)
+        binding = ActivityProductDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val productViewModel = ViewModelProvider(this).get(ProductViewModel::class.java)
+        val adapter = ProductAdapter()
+
+        productViewModel.products.observe(this) {
+            adapter.submitList(it)
+            binding.rvProduct.adapter = adapter
+        }
     }
 }
