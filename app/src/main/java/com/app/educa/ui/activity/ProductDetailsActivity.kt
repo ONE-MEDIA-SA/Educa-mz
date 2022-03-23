bd588e3dfe44fd4603734b17fbd6b39b70cc5b7a
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-23 11:51:05 +0200

    add activity page details

---
+++ app/src/main/java/com/app/educa/ui/activity/ProductDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.app.educa.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.educa.R
+
+class ProductDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
