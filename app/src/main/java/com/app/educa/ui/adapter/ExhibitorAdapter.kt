ca405965b3b048ae6a2ff5d910b323ec37da20b5
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-03-23 11:31:59 +0200

    add chat float button

--- app/src/main/java/com/app/educa/ui/adapter/ExhibitorAdapter.kt
+++ app/src/main/java/com/app/educa/ui/adapter/ExhibitorAdapter.kt
@@ -1,8 +1,10 @@
 package com.app.educa.ui.adapter
 
 import android.content.Intent
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,6 +12,7 @@ import com.app.educa.databinding.ItemExhibitorHorizontalBinding
 import com.app.educa.model.Exhibitor
 import com.app.educa.ui.activity.ExhibitorPageActivity
 import com.bumptech.glide.Glide
+import kotlin.random.Random
 
 class ExhibitorAdapter : ListAdapter<Exhibitor, ExhibitorAdapter.ExhibitorViewHolder>(Companion) {
 
@@ -33,9 +36,20 @@ class ExhibitorAdapter : ListAdapter<Exhibitor, ExhibitorAdapter.ExhibitorViewHo
         ))
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onBindViewHolder(holder: ExhibitorViewHolder, position: Int) {
         holder.binding.tvExhibitorName.text = currentList[position].name
         holder.binding.tvExhibitorCategory.text = currentList[position].category
+
+
+        holder.binding.btnExhibitorFavorite.setOnClickListener {
+            holder.binding.btnExhibitorFavorite.drawable.setTint(
+                holder
+                    .binding
+                    .btnExhibitorFavorite
+                    .context
+                    .getColor(android.R.color.holo_red_light))
+        }
 
         Glide
             .with(holder.binding.root)
