bf0b938fd863da2f874218f3f7d8ad57550c9c3b
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-04-12 09:51:09 +0200

    update exhibitors model

--- app/src/main/java/com/app/educa/model/Exhibitor.kt
+++ app/src/main/java/com/app/educa/model/Exhibitor.kt
@@ -4,6 +4,25 @@ import java.io.Serializable
 
 data class Exhibitor(
     var id: Int,
-    var name: String,
-    var category: String,
-    var profile: String) : Serializable
+    var name: String = "",
+    var email: String = "",
+    var description: String = "",
+    var category: String = "",
+    var contact: String = "",
+    var profile: String = "",
+    var cover: String = "",
+    var video: String = "",
+    var website: String = "",
+    var likes: List<String> = arrayListOf(),
+    var sector: String = "Privado",
+    var location: Location,
+    var isActive: Boolean = true
+
+) : Serializable
+
+data class Location(
+    var long: Long = 0,
+    var lat: Long = 0,
+    var address: String = "Maputo"
+)
+
